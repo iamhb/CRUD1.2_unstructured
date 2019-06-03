b90865e028b091cd61d6c38ae00c5f442494df10
iamhb <its.hari.programmer@gmail.com>
2019-06-03 18:05:40 +0530

    Final commit updated

--- hbangular/angular_demo/server.js
+++ hbangular/angular_demo/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const express =require('express');
-
 const app =express();
+
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
 
@@ -10,23 +10,23 @@ app.use(bodyParser.json());
 
 const path= require('path');
 
- 
- var modelVar = require('./models/DBmodel');
+var modelVar = require('./models/DBmodel');
 
 app.listen(3000,() =>{
 console.log("server listening the port 3000");
 });
 
+app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'js')));
 app.get('/', function(req, res) 
 	{
     	res.sendFile(path.join(__dirname + '/index.html'));
     	// app.use(express.static(path.join(__dirname, 'public')));
 	});  
+//app.get('/css')
 app.listen(8080);
-
    // create todo and send back all todos after creation
-
-    app.post('/api/todos', function(req, res) {
+app.post('/api/addData', function(req, res) {
     	console.log("in server api");
     	console.log("values:" + req.body);
 
@@ -40,14 +40,37 @@ app.listen(8080);
         modelVarObj.save((err)=>{
       		res.send(modelVarObj);
      	 	console.log("ADDED");
-  			 
-
-            // get and return all the todos after you create another
-           /* db.find(function(err, todos) {
-                if (err)
-                    res.send(err)
-                res.json(todos);
-            });*/
         });
-
     });
+
+app.get('/api/showData', function(req, res) {
+        var db = mongoose.connect('mongodb://localhost:27017/hbdbdemo' ,{ useNewUrlParser: true });
+        //var modelVarObj = new modelVar();
+        // use mongoose to get all todos in the database
+        modelVar.find(function(err, data) {
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err)
+            console.log(data);
+            res.json(data); // return all todos in JSON format
+        });
+    });
+
+
+app.post('/api/removeValue',function(req,res){
+  console.log("In Server Remove");
+  console.log("values:" + req.body.remVar);
+
+    modelVar.remove( {"fname": req.body.remVar },function (err, docs) {
+                    if(err)
+                        {
+                            res.status(500).json(err);
+                        }
+                    else if(docs)
+                        {
+                            console.log(docs);
+                            console.log("del successfully");
+                            res.status(200).json(docs);
+                        }
+});
+  });
